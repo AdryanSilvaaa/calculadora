5fe39a5ec86d42a3c3e0aa6d85db14653dffc276
Adryan Andre da Silva <silvaadryan366@gmail.com>
2024-08-29 20:25:40 -0300

    up
    nova, tarefa

--- calculadora.js
+++ calculadora.js
@@ -1,41 +1,43 @@
 
 function calculadora(valor1, valor2, tipoDeconta) {
-    var resultado;
+  var resultado;
+/* 
+  if (isNaN(valor1) || isNaN(valor2)) {
+    alert("Erro: apenas números são permitidos");
+    return;
+  } */
 
-    if (isNaN(valor1) || isNaN(valor2)) {
-      alert("Erro: apenas números são permitidos");
-      return;
-    }
-
-    if (tipoDeconta === "soma") {
-      resultado = valor1 + valor2;
-    } else if (tipoDeconta === "subtração") {
-      resultado = valor1 - valor2;
-    } else if (tipoDeconta === "divisão") {
-      resultado = valor1 / valor2;
-    } else if (tipoDeconta === "multiplicação") {
-      resultado = valor1 * valor2;
-    }
-
-    alert("O resultado é " + resultado);
+  if (tipoDeconta === "soma") {
+    resultado = valor1 + valor2;
+  } else if (tipoDeconta === "subtração") {
+    resultado = valor1 - valor2;
+  } else if (tipoDeconta === "divisão") {
+    resultado = valor1 / valor2;
+  } else if (tipoDeconta === "multiplicação") {
+    resultado = valor1 * valor2;
   }
 
-  var nu1 = prompt("Informe um valor");
-  if (isNaN(nu1) || nu1.trim() === "") {/* nu1.trim() === "" verifica se valor e vazio */
+  alert("O resultado é " + resultado);
+}
+
+var nu1 = prompt("Informe um valor");
+if (isNaN(nu1) || nu1.trim() === "") {/* nu1.trim() === "" verifica se valor e vazio */
+  alert("Erro: apenas números são permitidos");
+} else {
+  nu1 = parseFloat(nu1);
+
+  
+  var nu2 = prompt("Informe um valor");
+  if (isNaN(nu2) || nu2.trim() === "") {
     alert("Erro: apenas números são permitidos");
   } else {
-    nu1 = parseFloat(nu1);
-    var nu2 = prompt("Informe um valor");
-    if (isNaN(nu2) || nu2.trim() === "") {
-      alert("Erro: apenas números são permitidos");
-    } else {
-      nu2 = parseFloat(nu2);
-      var operacao = prompt(
-        "Que tipo de conta deseja fazer: soma, subtração, divisão, multiplicação"
-      );
-      calculadora(nu1, nu2, operacao);
-    }
+    nu2 = parseFloat(nu2);
+    var operacao = prompt(
+      "Que tipo de conta deseja fazer: soma, subtração, divisão, multiplicação"
+    );
+    calculadora(nu1, nu2, operacao);
   }
-  calculadora(nu1, nu2, operacao);
+}
+calculadora(nu1, nu2, operacao);
 
 52
---
+++ calculadora01.js
@@ -0,0 +1,38 @@
+/* 
+function farol( ) {
+    
+
+    switch (verde) {
+
+
+        case ("verde"):
+            break;
+        case ("vermelho"):
+            break;
+        case ("amarelo"):
+            break;
+        default:
+            break;
+ 
+    }
+}
+
+
+
+
+ */
+
+
+
+
+
+/* 
+ escrever uma função que recebera um parametro tipo texto pode ser verde vermelho ou amarelo.
+
+
+ a função ira testar o valor desse parametro tipo texto, caso o valor seja 
+ verde retornara um valor boolean que indica verdadeiro, e qualquer outra valor é falso. 
+ o valor por essa função devera ser armazenado em uma variavel e posteriormente sera testado.
+ se o valor for verdadeiro, mostrar uma menssagem de sucesso caso ao contrario indicar erro 
+
+*/
