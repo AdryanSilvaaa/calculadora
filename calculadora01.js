9f78fe9b50c8436eb306eb112b7c2cbe4ed07e6f
Adryan André Da Silva <125135703+Silvaadryan@users.noreply.github.com>
2024-09-05 16:58:09 -0300

    atividade da faculdade

--- calculadora01.js
+++ calculadora01.js
@@ -1,35 +1,34 @@
-/* 
-function farol( ) {
-    
 
-    switch (verde) {
-
-
-        case ("verde"):
-            break;
-        case ("vermelho"):
-            break;
-        case ("amarelo"):
+/* Função para verificar valor se é verde ou não  */
+function verificarCor(cor) {
+    let resultado;
+    switch (cor.toLowerCase()) {
+        case 'verde':
+            resultado = true;
             break;
         default:
-            break;
- 
+            resultado = false;
     }
+    return resultado;
 }
 
+// Solicitando o valor ao usuário com um prompt
+let corUsuario = prompt('Digite uma cor (verde, vermelho ou amarelo):');
 
+// Armazenando o valor retornado pela função em uma variável
+let corTeste = verificarCor(corUsuario);
 
-
- */
-
-
+// Testando o valor da variável e exibindo a mensagem correspondente
+if (corTeste) {
+    document.write('Sucesso: A cor é verde.');
+} else {
+    document.write('Erro');
+}
 
 
 
 /* 
  escrever uma função que recebera um parametro tipo texto pode ser verde vermelho ou amarelo.
-
-
  a função ira testar o valor desse parametro tipo texto, caso o valor seja 
  verde retornara um valor boolean que indica verdadeiro, e qualquer outra valor é falso. 
  o valor por essa função devera ser armazenado em uma variavel e posteriormente sera testado.
